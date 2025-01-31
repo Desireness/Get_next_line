3ea3d5fd003747311f1848ca8cd9cab1e19bdd3b
ralf <ralf@kali.sklor>
2025-01-31 08:23:11 +0100

    testsfd.c hace lectura de un buffer de 5 a un archivo llamado prueba.txt

--- testsfd.c
+++ testsfd.c
@@ -6,7 +6,7 @@
 /*   By: rauizqui <rauizqui@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 07:50:24 by rauizqui          #+#    #+#             */
-/*   Updated: 2025/01/31 08:02:54 by rauizqui         ###   ########.fr       */
+/*   Updated: 2025/01/31 08:20:16 by rauizqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 #include <string.h>
 #include <time.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 int main()
 {
@@ -27,8 +28,11 @@ int main()
 	char buff[256];
 	int	chars_read;
 
-	fd = open("file.txt", 0_RDONLY);
-
-	while((chars_read = read()))
+	fd = open("prueba.txt", O_RDONLY);
+	while((chars_read = read(fd, buff, 5)))
+	{
+		buff[chars_read] = '\0';
+		printf("buff->%s\n", buff);
+	}
 	
 }
