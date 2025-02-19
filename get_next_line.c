1d04f29b21813a5bb033fe3e71f12252cb1bcd67
ralf <ralf@kali.sklor>
2025-02-19 15:22:10 +0100

    Ahora si que no tiene leaks, ajustado con un buffer alojado con malloc, el anterior petaba con un buffer de mayor de 10millones, pasa paco

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rauizqui <rauizqui@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 22:37:00 by rauizqui          #+#    #+#             */
-/*   Updated: 2025/02/18 18:51:14 by rauizqui         ###   ########.fr       */
+/*   Updated: 2025/02/19 15:20:21 by rauizqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,28 +37,27 @@ void	*ft_calloc(size_t count, size_t size)
 	return (ptr);
 }
 
-char	*read_and_store(int fd, char **stash)
+char	*read_and_store(int fd, char **stash, char *buffer)
 {
-	char	buff[BUFFER_SIZE + 1];
 	int		chars_read;
 	char	*temp;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	chars_read = read(fd, buff, BUFFER_SIZE);
+	chars_read = read(fd, buffer, BUFFER_SIZE);
 	while (chars_read > 0)
 	{
-		buff[chars_read] = '\0';
+		buffer[chars_read] = '\0';
 		if (!(*stash))
 			*stash = ft_strdup("");
-		temp = ft_strjoin(*stash, buff);
+		temp = ft_strjoin(*stash, buffer);
 		if (!temp)
 			return (NULL);
 		free(*stash);
 		*stash = temp;
-		if (ft_strchr(buff, '\n'))
+		if (ft_strchr(buffer, '\n'))
 			break ;
-		chars_read = read(fd, buff, BUFFER_SIZE);
+		chars_read = read(fd, buffer, BUFFER_SIZE);
 	}
 	if (chars_read == -1)
 		return (free(*stash), *stash = NULL, NULL);
@@ -108,34 +107,42 @@ char	*extract_line(char **stash)
 char	*get_next_line(int fd)
 {
 	static char	*stash;
+	static char	*buffer;
 	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!read_and_store(fd, &stash))
+	if (!buffer)
+		buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer || !read_and_store(fd, &stash, buffer))
+	{
+		free(buffer);
+		buffer = NULL;
 		return (NULL);
+	}
 	line = extract_line(&stash);
 	if (!line)
 	{
 		free(stash);
-		return (NULL);
+		free(buffer);
+		stash = NULL;
+		buffer = NULL;
 	}
 	return (line);
 }
 
-//int main()
-//{
-//    int fd = open("prueba.txt", O_RDONLY);
-//    char *line;
-//
-//    if (fd < 0)
-//        return (1);
-//    while ((line = get_next_line(fd)))
-//    {
-//        printf("%s", line);
-//        free(line);
-//    }
-//    close(fd);
-//    return (0);
-//}
-//
+/*int main()
+{
+    int fd = open("prueba.txt", O_RDONLY);
+    char *line;
+
+    if (fd < 0)
+       return (1);
+   while ((line = get_next_line(fd)))
+    {
+        printf("%s", line);
+        free(line);
+    }
+    close(fd);
+    return (0);
+}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rauizqui <rauizqui@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 22:37:16 by rauizqui          #+#    #+#             */
-/*   Updated: 2025/02/18 19:11:30 by rauizqui         ###   ########.fr       */
+/*   Updated: 2025/02/19 14:50:12 by rauizqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
+#  define BUFFER_SIZE 10000000
 # endif
 
 # include <stdio.h>
@@ -25,6 +25,8 @@
 //funciones usadas en el GNL
 
 char	*get_next_line(int fd);
+char	*extract_line(char **stash);
+char	*read_and_store(int fd, char **stash, char *buffer);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
